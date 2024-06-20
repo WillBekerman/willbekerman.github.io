ec561c943a07bcf1072f70134194c812ac11d258
William Bekerman <wjb239@cornell.edu>
2024-06-19 23:14:15 -0400

    try again w abstracts

--- abstractscript.js
+++
@@ -1,9 +0,0 @@
-// Abstract button handling 
-function showAbstract(abstractId) {
-  var paragraph = document.getElementById(abstractId);
-  if (paragraph.style.display === "none") {
-    paragraph.style.display = "block";
-  } else {
-    paragraph.style.display = "none";
-  }
-}
